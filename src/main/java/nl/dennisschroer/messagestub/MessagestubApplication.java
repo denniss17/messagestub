2b8bef6c3bc187a40232a9a0b1aae67dc63ea53d
Dennis Schroer <dev@dennisschroer.nl>
2019-09-07 19:00:13 +0200

    Initial commit with Spring Initializr project

---
+++ src/main/java/nl/dennisschroer/messagestub/MessagestubApplication.java
@@ -0,0 +1,13 @@
+package nl.dennisschroer.messagestub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagestubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagestubApplication.class, args);
+	}
+
+}
