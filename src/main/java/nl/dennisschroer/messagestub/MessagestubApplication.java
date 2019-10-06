c88435e4a4e8099da95c5d760f4722eba2cc19fc
Dennis Schroer <dev@dennisschroer.nl>
2019-10-06 17:23:14 +0200

    WIP op mock-server

--- src/main/java/nl/dennisschroer/messagestub/MessagestubApplication.java
+++ src/main/java/nl/dennisschroer/messagestub/MessagestubApplication.java
@@ -1,15 +1,33 @@
 package nl.dennisschroer.messagestub;
 
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.mockserver.model.XmlSchemaBody.xmlSchema;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class MessagestubApplication {
+	private ClientAndServer mockServer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagestubApplication.class, args);
 	}
 
+	@PostConstruct
+	public void startMockServer(){
+		mockServer = ClientAndServer.startClientAndServer(1080);
+		mockServer.when(HttpRequest.request().withBody(xmlSchema("")))
+	}
+
+	@PreDestroy
+	public void stopMockServer(){
+		mockServer.stop();
+	}
 }
